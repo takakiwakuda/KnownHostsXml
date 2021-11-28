d5bb2bb833e3f256db452455f3e70e4741be2aa6
Takaki Wakuda <taka130102@hotmail.co.jp>
2021-11-28 12:19:40 +0900

    Add aliases for KnownHost

--- src/KnownHostsXml/KnownHost.cs
+++ src/KnownHostsXml/KnownHost.cs
@@ -5,18 +5,18 @@ namespace KnownHostsXml;
 /// <summary>
 /// Represents a record in known_hosts.
 /// </summary>
-[XmlRoot]
+[XmlRoot("host")]
 public sealed record KnownHost
 {
     /// <summary>
     /// Gets or sets the host name of the SSH host.
     /// </summary>
-    [XmlAttribute]
+    [XmlAttribute("name")]
     public string? HostName { get; init; }
 
     /// <summary>
     /// Gets or sets the fingerprint of the SSH host key.
     /// </summary>
-    [XmlAttribute]
+    [XmlAttribute("fingerprint")]
     public string? Fingerprint { get; init; }
 }
